a7eb74443b3337a8d5af84b2dff9a438c4d95192
Katya01234 <164415315+Katya01234@users.noreply.github.com>
2024-12-05 09:55:19 +0300

    Update task3.c
    Николаева Екатерина Юрьевна

--- 3824Б1ФИ3/21 Николаева Екатерина Юрьевна/task3.c
+++ 3824Б1ФИ3/21 Николаева Екатерина Юрьевна/task3.c
@@ -1,5 +1,7 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
 double max_of_two_numbers(double a, double b)
 {
+  if (a > b) printf("Maximum %f", a);
+  else printf("Maximum %f", b)
   return 0.0;
 }
